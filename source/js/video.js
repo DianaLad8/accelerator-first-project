a618c7b0251eb4e9cf455e20714831bc5d9594db
DianaPavlova <diana50888@gmail.com>
2024-03-17 11:10:27 +0200

    Добавляет блок супер-игры, добавляет миксин градиент кнопкам.

--- source/js/video.js
+++ source/js/video.js
@@ -5,25 +5,22 @@ const videoPlayer = document.querySelector('.video__iframe');
 const initVideo = () => {
 
   playButton.addEventListener('click', () => {
+
     const loadPlayer = videoPlayer.dataset.src;
     if (loadPlayer) {
       videoPlayer.innerHTML = `<iframe width="560" height="315" src="${loadPlayer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
       videoPlayer.removeAttribute('data-src');
     }
 
-    setTimeout(() => {
-      videoFond.style.opacity = 0.5;
-      playButton.style.opacity = 0.5;
-    }, 300);
+    const videoIframe = videoPlayer.querySelector('iframe');
+    videoIframe.src += '&autoplay=1';
 
     setTimeout(() => {
       videoFond.style.opacity = 0;
       playButton.style.opacity = 0;
+      videoFond.style.display = 'none';
+      playButton.style.display = 'none';
     }, 600);
-
-    const videoIframe = videoPlayer.querySelector('iframe');
-
-    videoIframe.src += '&autoplay=1';
   });
 };
 
